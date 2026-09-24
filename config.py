c6627473c1ab9f3f3de821826e7dd37e3875537d
Rohith <149065101+Ravanog@users.noreply.github.com>
2026-09-24 18:45:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from os import environ
 # Telegram API Credentials (Replace values or use Koyeb Environment Variables)
 API_ID = int(os.environ.get("API_ID", 15671595))
 API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token_here")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8932762816:AAHH4bYkyQ1a-Ujcbgsl5isu026GjIQ6wUI")
 
 # Start Message Picture (Direct Image URL)
-START_PIC = "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?w=800"
+START_PIC = "https://files.catbox.moe/9oc9ai.jpg"
 
 # Required members count to unlock movie searches
 REQUIRED_INVITES = 3
