6588512c55479f02e2882216747b5d4f6fb028e9
Rohith <149065101+Ravanog@users.noreply.github.com>
2026-09-25 16:12:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from os import environ
 # Telegram API Credentials (Replace values or use Koyeb Environment Variables)
 API_ID = int(os.environ.get("API_ID", 15671595))
 API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8932762816:AAHH4bYkyQ1a-Ujcbgsl5isu026GjIQ6wUI")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8916131861:AAHx8KQVvcUrXJkicyWSIlxmxRfX3StHgqQ")
 
 # Global Bot Admins / Owners (Put your Telegram User ID(s) here)
 ADMINS = [int(x) for x in os.environ.get("ADMINS", "8363515444").split(",") if x.strip().isdigit()]
