3386bc099fe77789e3c098835d0c3438df924502
Rohith <149065101+Ravanog@users.noreply.github.com>
2026-09-25 15:28:36 +0530

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,4 @@
+# All Codes Done By @HariKushal 
+# t.me/Harikushal
+# github.com/Kushalhk 
+# Dont remove My Credits if you born to your father only you will not remove my credit. If you born for me then you can edit the repo or remove the credits
